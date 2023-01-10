9a1901dc7cd1dbc01d0c5ef2db62b95bbdbbf9a9
Valery Sannikov <Valery_Sannikov@epam.com>
2023-01-10 10:45:30 +0400

    first commit

---
+++ cls.py
@@ -0,0 +1,15 @@
+class SchoolMember:
+
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
+    def present_yourself(self):
+        return f'My name in {self.name},i am {self.age} old'
+
+    @classmethod
+    def get_older(cls):
+        return f'My name in {cls.name},i am {cls.age + 1} old'
+
+
+
